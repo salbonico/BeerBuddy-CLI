15e0578fa62666dfee8bef6fe76696cf54aa0ce9
Steve Albonico <salbonico@gmail.com>
2019-01-24 17:41:04 +0000

    adding test urls to scrapper

--- lib/Scrapper.rb
+++ lib/Scrapper.rb
@@ -3,12 +3,18 @@ require_relative '../config/environment'
 class Scrapper
 
 
-  def self.scrape_index_page(index_url)
+  def self.scrape_beers(index_url)
    html = open(index_url)
    doc = Nokogiri::HTML(html)
    binding.pry
 end
 
+
+##testing url for beers
 self.scrape_index_page("https://www.taphunter.com/search/?type=beers&near=94611")
+beer_test_url = https://www.taphunter.com/search/?type=beers&near=94611
+##testing url for locations
+location_test_url = "https://www.taphunter.com/search/?type=locations&near=94611"
+
 
 end
