fc4ecd60c4d58723bdfab5a7c4774afdc307ac98
Steve Albonico <salbonico@gmail.com>
2019-01-24 17:12:26 +0000

    building dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 require 'bundler'
 Bundler.require
 require 'pry'
-
+require 'nokogiri'
+require 'open_uri_redirections'
 
 require_all 'lib'
--- lib/Scrapper.rb
+++ lib/Scrapper.rb
@@ -1,8 +1,14 @@
+require_relative '../config/environment'
+
 class Scrapper
 
 
+  def self.scrape_index_page(index_url)
+   html = open(index_url)
+   doc = Nokogiri::HTML(html)
+   binding.pry
+end
 
-
-
+self.scrape_index_page("https://www.taphunter.com/search/?type=beers&near=94611")
 
 end
