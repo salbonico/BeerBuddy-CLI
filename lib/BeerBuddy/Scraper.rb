0981aa36780bde8ee79ef2f3fc32839629a78115
Steve Albonico <salbonico@gmail.com>
2019-01-28 20:45:01 +0000

    Scrapper code tabbing fix and removed return

--- lib/BeerBuddy/Scraper.rb
+++ lib/BeerBuddy/Scraper.rb
@@ -2,14 +2,10 @@
 
 class Scraper
 
-
    def self.scrape_beers(url)
      html = open(url)
      doc = Nokogiri::HTML(html)
      output_of_hashes = []
-
-
-
 
      #beer name scrape
       imported_names = []
@@ -25,40 +21,41 @@ class Scraper
 
       #beer APV scrape
       imported_apvs = []
-        doc.css("p:nth-child(2) span:nth-child(2)").each do |apv|
-          imported_apvs << apv.text
+      doc.css("p:nth-child(2) span:nth-child(2)").each do |apv|
+        imported_apvs << apv.text
       end
 
       #beer IBU scrape
       imported_ibus = []
-        doc.css("p:nth-child(2) span:nth-child(3)").each do |ibu|
-          imported_ibus << ibu.text
+      doc.css("p:nth-child(2) span:nth-child(3)").each do |ibu|
+        imported_ibus << ibu.text
       end
 
       #beer brewer scrape
       imported_brewers = []
-        doc.css("p:nth-child(3) span:nth-child(1)").each do |brewer|
-          imported_brewers << brewer.text
+      doc.css("p:nth-child(3) span:nth-child(1)").each do |brewer|
+        imported_brewers << brewer.text
       end
 
       #beer description scrape
       imported_descriptions = []
-        doc.css("em").each do |description|
-          imported_descriptions << description.text
+      doc.css("em").each do |description|
+        imported_descriptions << description.text
       end
 
 
       imported_names.each.with_index do |name,index|
-      hash_entry = {}
-      hash_entry[:name] = name
-      hash_entry[:type] = imported_types[index]
-      hash_entry[:apv] = imported_apvs[index]
-      hash_entry[:ibu] = imported_ibus[index]
-      hash_entry[:brewer] = imported_brewers[index]
-      hash_entry[:description] = imported_descriptions[index]
-      output_of_hashes << hash_entry
+        hash_entry = {}
+        hash_entry[:name] = name
+        hash_entry[:type] = imported_types[index]
+        hash_entry[:apv] = imported_apvs[index]
+        hash_entry[:ibu] = imported_ibus[index]
+        hash_entry[:brewer] = imported_brewers[index]
+        hash_entry[:description] = imported_descriptions[index]
+        output_of_hashes << hash_entry
       end
-      return output_of_hashes
+
+    output_of_hashes
     end
 
 
